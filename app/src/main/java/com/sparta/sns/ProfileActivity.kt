186c391fa74801b22ee91b55a6ebf876ae9d6c25
Choi YoonChan <choyc9239@gmail.com>
2024-04-01 18:58:10 +0900

    [feat/#1] 마이페이지 id, pw 값이 null일때 데이터 보여주기

--- app/src/main/java/com/sparta/sns/ProfileActivity.kt
+++ app/src/main/java/com/sparta/sns/ProfileActivity.kt
@@ -38,8 +38,10 @@ class ProfileActivity : AppCompatActivity() {
 
     private fun setUpData() {
         val intent = intent
-        tvName.text = "이름: " + intent.getStringExtra("name")
-        tvId.text = "아이디: " + intent.getStringExtra("id")
+        val id = intent.getStringExtra("name")?: "입력된 내용이 없습니다."
+        val pw = intent.getStringExtra("id")?: "입력된 내용이 없습니다."
+        tvName.text = "이름: $id"
+        tvId.text = "아이디: $pw"
     }
 
     private fun setUpBackButtonListener() {
