2d7474a6d1562d840513695fb3d272afc6222dd0
kory0115 <kory01167@gmail.com>
2024-04-02 16:07:09 +0900

    [feat/2]로그인 부분 수정

--- app/src/main/java/com/sparta/sns/LoginActivity.kt
+++ app/src/main/java/com/sparta/sns/LoginActivity.kt
@@ -3,19 +3,38 @@ package com.sparta.sns
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import com.google.android.material.internal.TextWatcherAdapter
-import com.sparta.sns.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityLoginBinding.inflate( layoutInflater ) }
-    var id: String? = ""
+    /*var id: String? = ""
     var name: String? = ""
-    var password: String? = ""
+    var password: String? = ""*/
+    private lateinit var data: UserEntity
+    private val dataList = ArrayList<UserEntity>()
+
+    private val idEditTextView: EditText by lazy {
+        findViewById(R.id.idEditTextView)
+    }
+
+    private val loginButton: AppCompatButton by lazy {
+        findViewById(R.id.loginButton)
+    }
+
+    private val registerButton: AppCompatButton by lazy {
+        findViewById(R.id.registerButton)
+    }
+
+    private val passwordEditTextView: EditText by lazy {
+        findViewById(R.id.passwordEditTextView)
+    }
 
     private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             result: ActivityResult ->
@@ -24,62 +43,89 @@ class LoginActivity : AppCompatActivity() {
         }
 
         if(result.resultCode == Activity.RESULT_OK) {
-            id = result.data?.getStringExtra(MY_LOGIN_ID)
+            /*id = result.data?.getStringExtra(MY_LOGIN_ID)
             password = result.data?.getStringExtra(MY_LOGIN_PASSWORD)
-            name = result.data?.getStringExtra(MY_LOGIN_NAME)
-            binding.idEditTextView.setText(id)
+            name = result.data?.getStringExtra(MY_LOGIN_NAME)*/
+
+            data = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                result.data?.getParcelableExtra(MY_LOGIN_DATA, UserEntity::class.java)!!
+            } else {
+                (result.data?.getParcelableExtra(MY_LOGIN_DATA) as? UserEntity)!!
+            }
+
+            dataList += data
+            idEditTextView.setText(data.id)
         } else {
-            Toast.makeText(this, "수신 실패", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "입력값이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_login)
 
         bindView()
         initView()
     }
 
     private fun initView() {
-        binding.passwordEditTextView.addTextChangedListener(@SuppressLint("RestrictedApi")
+        passwordEditTextView.addTextChangedListener(@SuppressLint("RestrictedApi")
         object : TextWatcherAdapter() {
 
             override fun onTextChanged(s: CharSequence, p1: Int, p2: Int, p3: Int) {
-                if(binding.idEditTextView.length() > 0 && binding.passwordEditTextView.length() > 0) {
-                    binding.loginButton.isClickable = true
-                    binding.loginButton.isEnabled = true
-                    binding.loginButton.setBackgroundResource(R.drawable.button_background2)
+                if(idEditTextView.length() > 0 && passwordEditTextView.length() > 0) {
+                    loginButton.isClickable = true
+                    loginButton.isEnabled = true
+                    loginButton.setBackgroundResource(R.drawable.button_background2)
                 } else {
-                    binding.loginButton.isEnabled = false
-                    binding.loginButton.isClickable = false
+                    loginButton.isEnabled = false
+                    loginButton.isClickable = false
                 }
             }
         })
-        binding.idEditTextView.addTextChangedListener(@SuppressLint("RestrictedApi")
+        idEditTextView.addTextChangedListener(@SuppressLint("RestrictedApi")
         object : TextWatcherAdapter() {
 
             override fun onTextChanged(s: CharSequence, p1: Int, p2: Int, p3: Int) {
-                if(binding.passwordEditTextView.length() > 0 && binding.idEditTextView.length() > 0) {
-                    binding.loginButton.isClickable = true
-                    binding.loginButton.isEnabled = true
-                    binding.loginButton.setBackgroundResource(R.drawable.button_background2)
+                if(passwordEditTextView.length() > 0 && idEditTextView.length() > 0) {
+                    loginButton.isClickable = true
+                    loginButton.isEnabled = true
+                    loginButton.setBackgroundResource(R.drawable.button_background2)
                 } else {
-                    binding.loginButton.isClickable = true
-                    binding.loginButton.isClickable = false
+                    loginButton.isClickable = false
+                    loginButton.isEnabled = false
                 }
             }
         })
     }
 
+    private fun comPare() : Boolean {
+        for(i in 0 until dataList.size) {
+            if(idEditTextView.text.toString() == dataList[i].id && passwordEditTextView.text.toString() == dataList[i].password) {
+                data.apply {
+                    UserEntity(
+                        id = dataList[i].id,
+                        email = dataList[i].email,
+                        password = dataList[i].password,
+                        name = dataList[i].name
+                    )
+                }
+                return true
+            } else {
+                continue
+            }
+        }
+        return false
+    }
 
-    private fun bindView() = with(binding){
+    private fun bindView() {
 
         loginButton.setOnClickListener {
-            if(idEditTextView.text.toString() == id && passwordEditTextView.text.toString() == password) {
+
+            if(comPare()) {
                 val intent = Intent(this@LoginActivity, MainActivity::class.java) //메인페이지로 이동
-                intent.putExtra("id", id)
-                intent.putExtra("name", name)
+                intent.putExtra("USER_DATA", data)
+                intent.putParcelableArrayListExtra("USER_DATA_LIST", dataList)
                 startActivity(intent)
             } else {
                 Toast.makeText(this@LoginActivity, "아이디나 비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show()
@@ -93,8 +139,9 @@ class LoginActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val MY_LOGIN_NAME = "myloginkey"
+        /*const val MY_LOGIN_NAME = "myloginkey"
         const val MY_LOGIN_ID = "myloginid"
-        const val MY_LOGIN_PASSWORD = "myloginpassword"
+        const val MY_LOGIN_PASSWORD = "myloginpassword"*/
+        const val MY_LOGIN_DATA = "mylogindata"
     }
 }
--- app/src/main/java/com/sparta/sns/RegisterActivity.kt
+++ app/src/main/java/com/sparta/sns/RegisterActivity.kt
@@ -3,32 +3,86 @@ package com.sparta.sns
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.sparta.sns.databinding.ActivityRegisterBinding
+import androidx.appcompat.widget.AppCompatButton
 
 class RegisterActivity : AppCompatActivity(){
-    private val binding by lazy { ActivityRegisterBinding.inflate( layoutInflater ) }
 
+    private val openButton: AppCompatButton by lazy {
+        findViewById(R.id.openButton)
+    }
+
+    private val compareButton: Button by lazy {
+        findViewById(R.id.compareButton)
+    }
+
+    private val compareEditId: EditText by lazy {
+        findViewById(R.id.compareEditId)
+    }
+
+    private val nameEditTextView: EditText by lazy {
+        findViewById(R.id.nameEditTextView)
+    }
+
+    private val idEditTextView: EditText by lazy {
+        findViewById(R.id.idEditTextView)
+    }
+
+    private val passwordEditTextView: EditText by lazy {
+        findViewById(R.id.passwordEditTextView)
+    }
+
+    private val mailEditTextView: EditText by lazy {
+        findViewById(R.id.mailEditTextView)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_register)
 
-        binding.openButton.setOnClickListener {
-            if(binding.nameEditTextView.text.toString().isEmpty()
-                || binding.idEditTextView.text.toString().isEmpty()
-                || binding.passwordEditTextView.text.toString().isEmpty()
+        initViews()
+        bindViews()
+    }
+
+    private fun initViews() {
+        openButton.setOnClickListener {
+            val data = UserEntity(
+                id = idEditTextView.text.toString(),
+                email = mailEditTextView.text.toString(),
+                password = passwordEditTextView.text.toString(),
+                name = nameEditTextView.text.toString()
+            )
+
+
+            if(nameEditTextView.text.toString().isEmpty()
+                || idEditTextView.text.toString().isEmpty()
+                || passwordEditTextView.text.toString().isEmpty()
+                || mailEditTextView.text.toString().isEmpty()
             ) {
-                Toast.makeText(this, "모든항목을 작성해주세요.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "모든 항목을 작성해 주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             setResult(Activity.RESULT_OK, Intent().apply {
-                putExtra(LoginActivity.MY_LOGIN_NAME, binding.nameEditTextView.text.toString())
-                putExtra(LoginActivity.MY_LOGIN_ID, binding.idEditTextView.text.toString())
-                putExtra(LoginActivity.MY_LOGIN_PASSWORD, binding.passwordEditTextView.text.toString())
+                /*putExtra(LoginActivity.MY_LOGIN_NAME, nameEditTextView.text.toString())
+                putExtra(LoginActivity.MY_LOGIN_ID, idEditTextView.text.toString())
+                putExtra(LoginActivity.MY_LOGIN_PASSWORD, passwordEditTextView.text.toString())*/
+                putExtra(LoginActivity.MY_LOGIN_DATA, data)
             })
             finish()
+        }
+    }
+
+    private fun bindViews() {
+        compareButton.setOnClickListener {
+            if(idEditTextView.text.toString() == compareEditId.text.toString()
+                && idEditTextView.text.toString().isNotEmpty()) {
+                compareButton.setBackgroundResource(R.drawable.button_compare)
+            } else {
+                Toast.makeText(this, "아이디가 중복되었거나 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
