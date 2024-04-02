178fc19f5c86979c2a0f83ecf88f553916367b49
t2023-m0108 <t2023-m0108@t2023-m0108ui-MacBookAir.local>
2024-04-02 15:00:39 +0900

    [feat#4] 구현 UI
    UI 완성 - 버튼 클릭 구현

--- app/src/main/java/com/sparta/sns/DetailPageActivity.kt
+++ app/src/main/java/com/sparta/sns/DetailPageActivity.kt
@@ -1,22 +1,37 @@
 package com.sparta.sns
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DetailPageActivity : AppCompatActivity() {
+
+    private val btLike: ImageView by lazy {
+        findViewById(R.id.bt_like)
+    }
+
+    private val tvLike: TextView by lazy {
+        findViewById(R.id.tv_like)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detailpage)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
+
+
         }
     }
 }
-//깃헙 모험...
-//깃헙 모험 2... -f...
