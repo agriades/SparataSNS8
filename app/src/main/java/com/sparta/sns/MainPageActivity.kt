2c42639dde2b655e033d8abec43c560a2d6c1206
Sooyoung <agriades23@gmail.com>
2024-04-02 17:01:06 +0900

    [feat] MainPageActivity 안에서 유저 버튼 불러오고, 임시로 생성한 MyPageActivity를 Intent로 연결해서 클릭 시 이동하도록 구현함.

--- app/src/main/java/com/sparta/sns/MainPageActivity.kt
+++ app/src/main/java/com/sparta/sns/MainPageActivity.kt
@@ -1,11 +1,23 @@
 package com.sparta.sns
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //USER 버튼을 불러옴
+        val myPageButton = findViewById<TextView>(R.id.btn_member_user)
+        //MyPageActivity로 이동하는 부분까지만 작성
+        val myPageIntent = Intent(this, MyPageActivity::class.java)
+        myPageButton.setOnClickListener{
+            startActivity(myPageIntent)
+        }
     }
+
+
 }
