975eabfe05be3a4e57c3d404818549bee328c40e
Choi YoonChan <choyc9239@gmail.com>
2024-04-03 19:07:25 +0900

    [feat/#17] 공통 키값 생성

---
+++ app/src/main/java/com/sparta/sns/Constants.kt
@@ -0,0 +1,15 @@
+package com.sparta.sns
+
+object Constants {
+    //Intent 키 값
+    const val USER_DATA_KEY = "user_data_key"
+    const val WRITER_KEY = "writer_key"
+    const val IMAGE_KEY = "image_key"
+    const val CONTENT_KEY = "content_key"
+
+    //게시물에 따른 이미지 key 값
+    const val POSTER_ONE = "poster_one"
+    const val POSTER_TWO = "poster_two"
+    const val POSTER_THREE = "poster_three"
+
+}
