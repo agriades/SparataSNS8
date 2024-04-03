6ef2f2f86eccfb9df7ceec4ee07292c9f4f24250
t2023-m0108 <t2023-m0108@t2023-m0108ui-MacBookAir.local>
2024-04-03 10:03:46 +0900

    [feat/#4] 추가 주석

--- app/src/main/java/com/sparta/sns/DetailPageActivity.kt
+++ app/src/main/java/com/sparta/sns/DetailPageActivity.kt
@@ -1,20 +1,40 @@
 package com.sparta.sns
 
-import android.media.Image
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class DetailPageActivity : AppCompatActivity() {
 
     private val btBack: ImageView by lazy {
         findViewById(R.id.bt_back)
     } // 뒤로가기 버튼
+
+    private val tvWriter: TextView by lazy {
+        findViewById(R.id.tv_writer)
+    } // 게시글 작성자
+
+    private val tvPost: TextView by lazy {
+        findViewById(R.id.tv_post)
+    } // 게시글 내용
+
+    private val tvComment1Writer: TextView by lazy {
+        findViewById(R.id.tv_comment1_writer)
+    } // 댓글1 작성자
+
+    private val tvComment1: TextView by lazy {
+        findViewById(R.id.tv_comment1)
+    } // 댓글1 내용
+
+    private val tvComment2Writer: TextView by lazy {
+        findViewById(R.id.tv_comment2_writer)
+    } // 댓글2 작성자
+
+    private val tvComment2: TextView by lazy {
+        findViewById(R.id.tv_comment2)
+    } // 댓글2 내용
 
     private val btLike: ImageView by lazy {
         findViewById(R.id.bt_like)
@@ -30,7 +50,7 @@ class DetailPageActivity : AppCompatActivity() {
 
     private val tvDislike: TextView by lazy {
         findViewById(R.id.tv_dislike)
-    } // 싫어요 버튼
+    } // 싫어요 숫자
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,24 +62,36 @@ class DetailPageActivity : AppCompatActivity() {
 
     private fun initView() {
 
-        var like = getString(R.string.str_like).toInt() // 기본 좋아요 숫자를 string.xml에서 관리하도록 했습니다
-        var dislike = getString(R.string.str_dislike).toInt()
+        var numLike = getString(R.string.str_like).toInt() // 기본 좋아요 숫자를 string.xml에서 관리하도록 했습니다.
+        var numDislike = getString(R.string.str_dislike).toInt()
 
         btLike.setOnClickListener {
-            like ++
-            tvLike.text = "$like"
+            numLike++
+            tvLike.text = "$numLike"
             Toast.makeText(this, getString(R.string.str_toast_like), Toast.LENGTH_SHORT).show()
-        } // 좋아요 버튼이 눌리면 좋아요 숫자가 1 증가하고 토스트 메세지를 띄우도록 구현했습니다
+        } // 좋아요 버튼이 눌리면 좋아요 숫자가 1 증가하고 토스트 메세지를 띄우도록 구현했습니다.
 
         btDislike.setOnClickListener {
-            dislike ++
-            tvDislike.text = "$dislike"
+            numDislike++
+            tvDislike.text = "$numDislike"
             Toast.makeText(this, getString(R.string.str_toast_dislike), Toast.LENGTH_SHORT).show()
         } // 싫어요 버튼도 좋아요 버튼과 같은 동작입니다
+
+
+        tvWriter.text = intent.getStringExtra("")
+        tvPost.text = intent.getStringExtra("")
+        tvComment1Writer.text = intent.getStringExtra("")
+        tvComment1.text = intent.getStringExtra("")
+        tvComment2Writer.text = intent.getStringExtra("")
+        tvComment2.text = intent.getStringExtra("")
+        // 게시글과 댓글의 작성자 및 내용을 intent를 통해 받아온 값으로 입력되도록 구현했습니다.
+
 
         btBack.setOnClickListener {
             finish()
         }
 
     }
+
+
 }
