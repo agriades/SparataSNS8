eca61beb4372f166ec43f4a10cee3f35bf8717fa
Sooyoung <agriades23@gmail.com>
2024-04-03 15:02:43 +0900

    [feat/#4] xml 아이디 btn_member -> tv_member_ 로 수정함

--- app/src/main/java/com/sparta/sns/MainPageActivity.kt
+++ app/src/main/java/com/sparta/sns/MainPageActivity.kt
@@ -15,10 +15,9 @@ class MainPageActivity : AppCompatActivity() {
         //유동 vs 고정? 4명을 고정하면 데이터 보낼 필요 없음. 준식님과 의논
 
         //우측 상단 버튼 id 불러옴
-        val myPageButton = findViewById<TextView>(R.id.btn_member_user)
+        val myPageButton = findViewById<TextView>(R.id.tv_member_user)
         myPageButton.text = "바뀜"  //여기서 회원가입 페이지에서 받은 이름 정보를 표기해야 합니다.
 
-        //
         val postWriterTextView = findViewById<TextView>(R.id.post_tv_writer)
         val writerName = "황수영" //저장된 게시글에서 해당되는 작성자 명을 불러와 주세요.
         val postWriterWho = resources.getString(R.string.post_writer_who) //" 님이 포스트를 올렸습니다."
