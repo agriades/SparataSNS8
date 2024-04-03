f42b965faba5b31785d178e239fbba91a680b91c
Sooyoung <agriades23@gmail.com>
2024-04-03 11:29:37 +0900

    [feat/#4] 회원가입 페이지에서 받은 이름 정보가 myPageButton.text에 떠야 하지만 아직 합치기 전이므로, "바뀜" 이라고 표시해 두었습니다.

--- app/src/main/java/com/sparta/sns/MainPageActivity.kt
+++ app/src/main/java/com/sparta/sns/MainPageActivity.kt
@@ -14,6 +14,8 @@ class MainPageActivity : AppCompatActivity() {
 
         //USER 버튼을 불러옴
         val myPageButton = findViewById<TextView>(R.id.btn_member_user)
+        //여기서 회원가입 페이지에서 받은 이름 정보를 표기해야 합니다.
+        myPageButton.text = "바뀜"
         //MyPageActivity로 이동하는 부분까지만 작성
         val myPageIntent = Intent(this, MyPageActivity::class.java)
         myPageButton.setOnClickListener{
