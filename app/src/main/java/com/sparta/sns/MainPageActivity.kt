9927701afdee319293c3d4ed1ccebff3f0fcf319
Sooyoung <agriades23@gmail.com>
2024-04-03 12:28:03 +0900

    [feat/#4] 최상단 게시글 하나의 "000 님이 포스트를 올렸습니다." 이름을 동적으로 처리할 수 있도록 변수 선언함.

--- app/src/main/java/com/sparta/sns/MainPageActivity.kt
+++ app/src/main/java/com/sparta/sns/MainPageActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class MainPageActivity : AppCompatActivity() {
 
@@ -13,10 +14,16 @@ class MainPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //유동 vs 고정? 4명을 고정하면 데이터 보낼 필요 없음. 준식님과 의논
 
-        //USER 버튼을 불러옴
+        //우측 상단 버튼 id 불러옴
         val myPageButton = findViewById<TextView>(R.id.btn_member_user)
-        //여기서 회원가입 페이지에서 받은 이름 정보를 표기해야 합니다.
-        myPageButton.text = "바뀜"
+        myPageButton.text = "바뀜"  //여기서 회원가입 페이지에서 받은 이름 정보를 표기해야 합니다.
+
+        //
+        val postWriterTextView = findViewById<TextView>(R.id.post_tv_writer)
+        val writerName = "황수영" //저장된 게시글에서 해당되는 작성자 명을 불러와 주세요.
+        val postWriterWho = resources.getString(R.string.post_writer_who) //" 님이 포스트를 올렸습니다."
+        postWriterTextView.text = writerName + postWriterWho
+
         //MyPageActivity로 이동하는 부분까지만 작성
         val myPageIntent = Intent(this, MyPageActivity::class.java)
         myPageButton.setOnClickListener{
